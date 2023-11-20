ce253a2e34d9c366afb6f7071cc36f924dc5c036
lsprgabriel <ogabrielpereira@pm.me>
2023-11-20 16:48:25 -0300

    Minimal updates

--- frontend/src/Pages/Edit.tsx
+++ frontend/src/Pages/Edit.tsx
@@ -85,13 +85,14 @@ export default function EditPage() {
                             <input defaultValue={user.email} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="teste@teste.com" required />
                         </div>
                     </div>
-                    <div className="flex flex-wrap mx-2 mb-6"></div>
+                    <div className="flex flex-wrap mx-2 mb-6">
                         <div className="w-full px-3">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="password">
                                 Senha
                             </label>
                             <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="password" type="password" placeholder="********" required />
                         </div>
+                    </div>
                     <div className="flex flex-wrap mx-2 mb-6">
                         <div className="w-full px-3">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="isAdmin">
